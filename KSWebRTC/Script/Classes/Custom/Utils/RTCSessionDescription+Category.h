a882cf7046508ebfa536f8da57850e20e2c55006
saeipi <saeipi@163.com>
2020-07-10 16:05:45 +0800

    Category

---
+++ KSWebRTC/Script/Classes/Custom/Utils/RTCSessionDescription+Category.h
@@ -0,0 +1,14 @@
+//
+//  RTCSessionDescription+Category.h
+//  KSWebRTC
+//
+//  Created by saeipi on 2020/7/10.
+//  Copyright © 2020 saeipi. All rights reserved.
+//
+
+
+#import "WebRTC/RTCSessionDescription.h"
+
+@interface RTCSessionDescription (Category)
+
+@end
---
+++ KSWebRTC/Script/Classes/Custom/Utils/RTCSessionDescription+Category.m
@@ -0,0 +1,32 @@
+//
+//  RTCSessionDescription+Category.m
+//  KSWebRTC
+//
+//  Created by saeipi on 2020/7/10.
+//  Copyright © 2020 saeipi. All rights reserved.
+//
+
+#import "RTCSessionDescription+Category.h"
+
+static NSString const *KSRTCSessionDescriptionTypeKey = @"type";
+static NSString const *KSRTCSessionDescriptionSdpKey = @"sdp";
+
+@implementation RTCSessionDescription (Category)
+
++ (RTCSessionDescription *)descriptionFromJSONDictionary:(NSDictionary *)dictionary {
+    NSString *typeString = dictionary[KSRTCSessionDescriptionTypeKey];
+    RTCSdpType type = [[self class] typeForString:typeString];
+    NSString *sdp = dictionary[KSRTCSessionDescriptionSdpKey];
+    return [[RTCSessionDescription alloc] initWithType:type sdp:sdp];
+}
+
+- (NSData *)JSONData {
+    NSString *type = [[self class] stringForType:self.type];
+    NSDictionary *json = @{
+            KSRTCSessionDescriptionTypeKey: type,
+            KSRTCSessionDescriptionSdpKey: self.sdp
+    };
+    return [NSJSONSerialization dataWithJSONObject:json options:0 error:nil];
+}
+
+@end
