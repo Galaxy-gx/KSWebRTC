1651bfbbf6221c713095218cf00877cddc40d92c
huchaojun <huchaojun@topgamesinc.com>
2020-08-17 11:03:59 +0800

    同步引用

--- KSWebRTC/Script/Chat/Custom/Import/KSImport.h
+++ KSWebRTC/Script/Chat/Custom/Import/KSImport.h
@@ -10,6 +10,19 @@
 #define KSImport_h
 
 #import "KSExtern.h"
+//
+////Category
+//#import "UIButton+Category.h"
+//#import "UIView+Category.h"
+//#import "UIImageView+Category.h"
+//#import "UIImage+Category.h"
+//#import "UILabel+Category.h"
+//#import "UIButton+Category.h"
+//#import "UIColor+Category.h"
+//#import "UIFont+Category.h"
+//#import "NSString+Category.h"
+//#import "KSSuperController+Category.h"
+//
 #import "KSMacro.h"
 
 #endif /* KSImport_h */
